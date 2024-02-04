571660df620d6b9f51327362ddf8ac817160e603
vaibhav-sutariya <vsutariya428@gmail.com>
2024-02-04 11:01:20 +0530

    main

---
+++ lib/components/belowAppbar.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+
+class AddressBox extends StatefulWidget {
+  const AddressBox({Key? key}) : super(key: key);
+
+  @override
+  State<AddressBox> createState() => _AddressBoxState();
+}
+
+class _AddressBoxState extends State<AddressBox> {
+  Position? _currentPosition;
+  String? _currentAddress;
+
+  @override
+  void initState() {
+    super.initState();
+    _getLocationPermission();
+  }
+
+  Future<void> _getLocationPermission() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Handle case when permission is denied
+        return;
+      }
+    }
+
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition();
+      setState(() {
+        _currentPosition = position;
+      });
+
+      // Reverse geocode the coordinates to get the address
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+        position.latitude,
+        position.longitude,
+      );
+
+      // Extract the address details
+      if (placemarks.isNotEmpty) {
+        Placemark placemark = placemarks.first;
+        _currentAddress =
+            "${placemark.subThoroughfare} ${placemark.thoroughfare}, "
+            "${placemark.subLocality}, ${placemark.locality}, ${placemark.postalCode}, "
+            "${placemark.country}";
+      } else {
+        _currentAddress = "Address not found";
+      }
+
+      setState(() {
+        _currentAddress = _currentAddress;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Color(0xFFFFA53E), Color(0xFFFF7643)],
+          stops: [0.5, 1.0],
+        ),
+      ),
+      padding: const EdgeInsets.only(left: 10),
+      child: Row(
+        children: [
+          const Icon(
+            Icons.location_on_outlined,
+            size: 30,
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (_currentPosition != null)
+                    Text(
+                      'Current Location: $_currentAddress',
+                      style: const TextStyle(fontSize: 15, color: Colors.black),
+                    ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/intiScreen.dart
+++ lib/screens/intiScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:no_hunger/components/belowAppbar.dart';
 import 'package:no_hunger/constants.dart';
 import 'package:no_hunger/screens/addFood/addFoodDetails.dart';
 import 'package:no_hunger/screens/main/bottomBar.dart';
@@ -18,91 +19,104 @@ class _InitScreenState extends State<InitScreen> {
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: kPrimaryColor,
-        title: const Text(
-          'Food Finder',
-          style: TextStyle(color: Colors.black, fontWeight: FontWeight.w600),
+        title: const Column(
+          children: [
+            Text(
+              'Food Finder',
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.w600),
+            ),
+          ],
         ),
       ),
-      body: Container(
-        // decoration: const BoxDecoration(
-        //   image: DecorationImage(
-        //       image: NetworkImage(
-        //           "https://c0.wallpaperflare.com/preview/116/193/176/blur-blurred-background-citrus-close-up.jpg"), // Replace with your image asset path
-        //       fit: BoxFit.cover,
-        //       opacity: 10),
-        // ),
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text('Are You Hungry??'),
-                ),
-                ElevatedButton.icon(
-                  icon: const Icon(Icons.restaurant),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      CupertinoPageRoute(
-                        builder: (context) => const BottomBar(),
-                      ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.black,
-                    backgroundColor: kPrimaryColor, // Text color
-                    padding: const EdgeInsets.all(16), // Button padding
-                    shape: RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.circular(8.0), // Rounded corners
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const AddressBox(),
+          Expanded(
+            child: Container(
+              // decoration: const BoxDecoration(
+              //   image: DecorationImage(
+              //       image: NetworkImage(
+              //           "https://c0.wallpaperflare.com/preview/116/193/176/blur-blurred-background-citrus-close-up.jpg"), // Replace with your image asset path
+              //       fit: BoxFit.cover,
+              //       opacity: 10),
+              // ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text('Are You Hungry??'),
                     ),
-                    elevation: 5, // Elevation (shadow)
-                    minimumSize: const Size(double.infinity, 0), // Full width
-                  ),
-                  label: const Text(
-                    'Find Food',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text('You have Remaining Food??'),
-                ),
-                ElevatedButton.icon(
-                  icon: const Icon(Icons.add),
-                  onPressed: () {
-                    // Add your button press logic here
-                    Navigator.push(
-                      context,
-                      CupertinoPageRoute(
-                        builder: (context) => const AddFoodDetails(),
+                    ElevatedButton.icon(
+                      icon: const Icon(Icons.restaurant),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          CupertinoPageRoute(
+                            builder: (context) => const BottomBar(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: kPrimaryColor, // Text color
+                        padding: const EdgeInsets.all(16), // Button padding
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(8.0), // Rounded corners
+                        ),
+                        elevation: 5, // Elevation (shadow)
+                        minimumSize:
+                            const Size(double.infinity, 0), // Full width
                       ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: Colors.green[700], // Text color
-                    padding: const EdgeInsets.all(16), // Button padding
-                    shape: RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.circular(8.0), // Rounded corners
+                      label: const Text(
+                        'Find Food',
+                        style: TextStyle(fontSize: 18),
+                      ),
                     ),
-                    elevation: 5, // Elevation (shadow)
-                    minimumSize: const Size(double.infinity, 0), // Full width
-                  ),
-                  label: const Text(
-                    'Add Food',
-                    style: TextStyle(fontSize: 18),
-                  ),
+                    const SizedBox(height: 16),
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text('You have Remaining Food??'),
+                    ),
+                    ElevatedButton.icon(
+                      icon: const Icon(Icons.add),
+                      onPressed: () {
+                        // Add your button press logic here
+                        Navigator.push(
+                          context,
+                          CupertinoPageRoute(
+                            builder: (context) => const AddFoodDetails(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.green[700], // Text color
+                        padding: const EdgeInsets.all(16), // Button padding
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(8.0), // Rounded corners
+                        ),
+                        elevation: 5, // Elevation (shadow)
+                        minimumSize:
+                            const Size(double.infinity, 0), // Full width
+                      ),
+                      label: const Text(
+                        'Add Food',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
